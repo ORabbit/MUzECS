d9fef62d632526fe3d21b89661056d0de8cfd4ed
Casey O <lionspride4@gmail.com>
2015-06-04 16:31:48 -0500

    Added play_frequency and play_frequency_time blocks

--- blockly/generators/arduino/ecs.js
+++ blockly/generators/arduino/ecs.js
@@ -18,6 +18,21 @@ Blockly.JavaScript['play_note_time'] = function(block) {
   return code;
 };
 
+Blockly.JavaScript['play_frequency'] = function(block) {
+  var value_frequency = Blockly.JavaScript.valueToCode(block, 'frequency', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
+Blockly.JavaScript['play_frequency_time'] = function(block) {
+  var value_frequency = Blockly.JavaScript.valueToCode(block, 'frequency', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_milliseconds = Blockly.JavaScript.valueToCode(block, 'milliseconds', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
 Blockly.JavaScript['turn_led_1_on'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
   var code = 'digitalWrite(5, HIGH);\n';
